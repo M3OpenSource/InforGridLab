b8d005431fa2c4561f93110b7e203f36cf7478f5
Thibaud Lopez Schneider <ThibaudLopez@users.noreply.github.com>
2017-05-25 04:33:54 -0700

    Update StartDefaultRouter.cmd

--- Lab5/StartDefaultRouter.cmd
+++ Lab5/StartDefaultRouter.cmd
@@ -1,3 +1,3 @@
 title Grid Default Router
 call SetClasspath.cmd
-java -cp %CLASSPATH% com.lawson.grid.Startup -router "Default Router" -configDir C:\Infor\Grid5\ -host localhost -logLevel ALL
+java -cp %CLASSPATH% com.lawson.grid.Startup -router "Default Router" -configDir C:\Infor\Grid\ -host localhost -logLevel ALL
