20c69d3f14d7a8df5e5e1cc973f5c0fd06642e41
Thibaud Lopez Schneider <ThibaudLopez@users.noreply.github.com>
2017-05-25 04:37:40 -0700

    Delete StartAdministrativeRouter.cmd

--- Lab5/StartAdministrativeRouter.cmd
+++
@@ -1,3 +0,0 @@
-title Grid Administrative Router
-call SetClasspath.cmd
-java -cp %CLASSPATH% com.lawson.grid.Startup -router "Administrative Router" -configDir C:\Infor\Grid\ -host localhost -logLevel ALL
