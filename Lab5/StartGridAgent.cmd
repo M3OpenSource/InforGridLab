4d1c4f5ed36b6674fda091e2934a0583c664038d
Thibaud Lopez Schneider <ThibaudLopez@users.noreply.github.com>
2017-05-25 04:31:05 -0700

    Add files via upload

---
+++ Lab5/StartGridAgent.cmd
@@ -0,0 +1,3 @@
+title Grid Agent
+call SetClasspath.cmd
+java -cp %CLASSPATH% com.lawson.grid.agent.GridAgent -configDir C:\Infor\Grid5\ -logToConsole -logLevel ALL -host localhost
---
+++ Lab5/StartRegistry.cmd
@@ -0,0 +1,3 @@
+title Grid Registry
+call SetClasspath.cmd
+java -cp %CLASSPATH% com.lawson.grid.Startup -registry -configDir C:\Infor\Grid5\ -host localhost -logLevel ALL
