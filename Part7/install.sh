7c9c4c529beaca6a98ae7a01948b9ed2576bdd23
Thibaud Lopez Schneider <ThibaudLopez@users.noreply.github.com>
2017-06-16 05:00:11 -0700

    Update install.sh

--- Part7/install.sh
+++ Part7/install.sh
@@ -3,14 +3,6 @@
 # Install the Infor ION Grid on a CentOS droplet at DigitalOcean
 # https://m3ideas.org/2017/06/13/building-an-infor-grid-lab-part-7/
 
-# Usage:
-# 1) Create a Downloads folder:
-#        ssh root@108.101.101.116 'mkdir ~/Downloads/'
-# 2) Copy the Grid installer to the droplet:
-#        scp ~/Downloads/Grid_Installer_11.1.13.0.77.lcm root@108.101.101.116:~/Downloads/
-# 3) Download and execute this installation script:
-#        ssh root@108.101.101.116 'curl https://raw.githubusercontent.com/M3OpenSource/InforGridLab/master/Part7/install.sh | bash -s'
-# Note: change the droplet's public IP address accordingly, e.g. 108.101.101.116
 export INSTALLER=~/Downloads/Grid_Installer_11.1.13.0.77.lcm
 if (test ! -f "$INSTALLER"); then
 	echo -e "Missing $INSTALLER"
@@ -18,43 +10,38 @@ if (test ! -f "$INSTALLER"); then
 fi
 
 # install the JDK
-yum --assumeyes install java-1.8.0-openjdk-devel
-
-# unzip the installer
-cd ~/Downloads/
-jar xvf $INSTALLER
-cd ~
+sudo yum --assumeyes install java-1.8.0-openjdk-devel
 
 # install PostgreSQL
-yum --assumeyes install postgresql-server
-postgresql-setup initdb
+sudo yum --assumeyes install postgresql-server
+sudo postgresql-setup initdb
 
 # setup password authentication for PostgreSQL
-sed --in-place \
+sudo sed --in-place \
 -e "s/host    all             all             127.0.0.1\/32            ident/host    all             all             127.0.0.1\/32            md5/" \
 -e "s/host    all             all             ::1\/128                 ident/host    all             all             ::1\/128                 md5/" \
 /var/lib/pgsql/data/pg_hba.conf
 
 
 # start and enable PostgreSQL
-systemctl start postgresql
-systemctl enable postgresql
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
 
 # set the PostgreSQL password
-sudo -u postgres psql -c "ALTER USER postgres with encrypted password 'password123';"
+sudo -i -u postgres psql -c "ALTER USER postgres with encrypted password 'password123';"
 
 # create the Grid database
-sudo -u postgres createdb InforIONGrid
+sudo -i -u postgres createdb InforIONGrid
 
 # create the user and group for the Grid service
-groupadd grid
-useradd -g grid grid
+sudo groupadd grid
+sudo useradd -g grid grid
 
 # get the droplet's external IP address
 IPADDR=$(ifconfig eth0 | awk '/inet /{print substr($2,1)}')
 
 # create the installer.properties
-java -jar ~/Downloads/products/Infor_ION_Grid_11.1.13.0/components/installer-1.13.77.jar -console -options-template ~/Downloads/installer.properties
+java -jar ~/Downloads/installer-1.13.77.jar -console -options-template ~/Downloads/installer.properties
 
 # change the properties
 sed --in-place \
@@ -72,4 +59,4 @@ sed --in-place \
 ~/Downloads/installer.properties
 
 # install the Grid
-java -jar ~/Downloads/products/Infor_ION_Grid_11.1.13.0/components/installer-1.13.77.jar -console -options ~/Downloads/installer.properties
+sudo java -jar ~/Downloads/installer-1.13.77.jar -console -options ~/Downloads/installer.properties
